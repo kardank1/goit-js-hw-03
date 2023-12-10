6da100a46c08359b0a7742ced268af8f7854e5da
kardank1 <kaplun.danil.2001@gmail.com>
2023-12-10 15:06:56 +0200

    1

---
+++ js/task-2.js
@@ -0,0 +1,11 @@
+function makeArray(firstArray, secondArray, maxLength){
+    const arrAll = firstArray.concat(secondArray);
+    return arrAll.length>maxLength ? arrAll.slice(0, maxLength) : arrAll;
+}
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
